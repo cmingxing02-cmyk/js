9600dcdb99f0e14c67d4fde0a189cafb9b6b8817
cmingxing02-cmyk <cmingxing02@gmail.com>
2026-01-01 09:15:25 +0800

    Update pg.js

--- pg.js
+++ pg.js
@@ -1,49 +1,31 @@
 var rule = {
-    title: '修罗影视（下载源）',
-    host: 'https://xl01.com.de',
-    url: '/s/fyclass',
-    searchUrl: '/s/search?wd=**',
-
+    title: '剧梦屋',
+    host: 'https://www.jumengwu.com',
+    // homeUrl: '/',
+    url: '/index.php/vod/type/id/fyclass.html?page=fypage',
+    searchUrl: '/index.php/vod/search.html?wd=**&page=fypage',
     searchable: 2,
     quickSearch: 1,
     filterable: 0,
-
-    class_name: '最新电影&最新剧集&国产剧&港台剧&动漫&欧美剧&日韩剧',
-    class_url: 'all?type=0&all?type=1&guoju&gangtaiju&donghua?type=1&meiju&hanju',
-
-    // 一级：纯文本抓取（标题从a标签，备注从日期/评分）
-    一级: 'js:' +
-        'let html = request(input);' +
-        'let d = [];' +
-        'let lists = pdfa(html, "body && .list");' +  // 假设有.list或用正则
-        'pdfa(html, "a").forEach(a => {' +
-        '    let title = pdfh(a, "a&&Text");' +
-        '    if(title && a.attr("href").startsWith("/")) {' +
-        '        d.push({' +
-        '            title: title,' +
-        '            pic_url: "",' +  // 无图
-        '            desc: "下载资源",' +
-        '            url: "https://xl01.com.de" + a.attr("href"),' +
-        '            content: "磁力/迅雷下载"' +
-        '        });' +
-        '    }' +
-        '});' +
-        'setResult(d);',
-
-    // 二级：抓简介 + 下载链接
-    二级: 'js:' +
-        'let html = request(input);' +
-        'VOD = {};' +
-        'VOD.vod_name = pdfh(html, "h1&&Text") || "未知";' +
-        'VOD.vod_pic = "";  // 无图
-        'VOD.vod_content = pdfh(html, ".intro&&Text") || "无简介";' +
-        'VOD.vod_play_from = "磁力下载$迅雷下载";' +
-        'let mags = pdfa(html, "a[href^=magnet]");' +
-        'let eds = pdfa(html, "a[href^=ed2k]");' +
-        'let playUrl = mags.map(a => a.attr("href")).join("#") + "$" + eds.map(a => a.attr("href")).join("#");' +
-        'VOD.vod_play_url = playUrl || "无下载链接";',
-
-    lazy: 'js:' +
-        'print("下载链接：" + input);' +  // 点播放显示链接，复制用下载器
-        'input = {parse:0, url: "", jx:0};'
-};
+    headers: {
+        'User-Agent': 'MOBILE_UA',
+    },
+    class_name: '电影&电视剧&动漫&综艺&纪录片&短剧',
+    class_url: '1&2&3&4&5&6',
+    cate_exclude: '',
+    play_parse: true,
+    lazy: '',
+    limit: 20,
+    推荐: '*',
+    double: false,
+    一级: 'a[href^="/index.php/vod/detail/id/"]:lt(30);a&&Text;a&&href;img&&data-original||src;.update-time&&Text',  // 首页/分类列表提取（标题;链接;图片;备注）
+    二级: {
+        "title": "h1&&Text;.data--span:eq(0)&&Text",  // 标题 + 年份/地区等
+        "img": ".picture img&&src",
+        "desc": ".data--span:eq(1)&&Text;;.data--span:eq(2)&&Text;.data--span:eq(3)&&Text;.data--span:eq(4)&&Text",
+        "content": ".desc p&&Text",
+        "tabs": ".play-source-tabs a",
+        "lists": ".playlist li:eq(#id) a:eq(1);30"
+    },
+    搜索: '#search-list .video-item;a&&Text;a&&href;img&&data-original||src;.update-time&&Text',
+}
